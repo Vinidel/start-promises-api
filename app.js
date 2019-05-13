3e174ad6abf17785d933f1e51e13aa3da9b157a9
Vinicius Delascio Fortunato <vinidroid@gmail.com>
2019-05-13 22:32:04 +1000

    Add cors

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors')
 const predictionsRoute = require('./routes/predictions');
 
 const app = express();
 
 // view engine setup
+app.use(cors());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
